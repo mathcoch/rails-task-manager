b964ab61fadb618b60dddb128fcca4a9d6a4f04c
mathcoch <mathieu@cochet.me>
2017-05-16 14:26:23 +0200

    Exo 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  root :to => 'tasks#index'
+
+  # get 'tasks' => 'tasks#index', :as => 'tasks'
+  # get 'tasks/new' => 'tasks#new', :as => 'new_task'
+  # post 'tasks' => 'tasks#create'
+  # get 'tasks/:id' => 'tasks#show', :as => 'task'
+  # get 'tasks/:id/edit' => 'tasks#edit', :as => 'edit_task'
+  # patch 'tasks/:id' => 'tasks#update'
+  # delete 'tasks/:id' => 'tasks#destroy'
+
 end
